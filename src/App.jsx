ccb79354a37bb3080767c4c66978142eba0efd8d
zikrya <77081809+zikrya@users.noreply.github.com>
2023-10-11 12:04:57 -0400

    [ADDED] React-router dom is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import ChatGpt from './components/ChatGpt'
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
 
   return (
     <>
-    <p className='text-2xl'>Senior Design</p>
-    <br />
-    <br />
-    <ChatGpt/>
+        <Router>
+          <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/register" element={
+          <Register/>} />
+           <Route exact path="/register" element={<Dashboard/>} />
+          </Routes>
+        </Router>
     </>
   )
 }
