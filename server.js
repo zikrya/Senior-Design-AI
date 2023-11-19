39a3bbe445cdf79e9d994267b63ef6fc824feee9
zikrya <77081809+zikrya@users.noreply.github.com>
2023-11-18 21:13:57 -0500

    [ADDED] db connected to user questions

--- server.js
+++ server.js
@@ -139,6 +139,49 @@ app.post('/logout', (req, res) => {
   res.json({ message: 'Logout successful' });
 });
 
+app.post('/save-questions', authenticateToken, async (req, res) => {
+  try {
+    const { questions } = req.body;
+    const userId = req.user.userId; // Extract userId from the authenticated user
+
+    await Promise.all(questions.map(async (question) => {
+      await prisma.userQuestions.create({
+        data: {
+          userId: userId,
+          questionText: question.question_text,
+          options: question.options,
+          correctOptionIndex: question.correct_option_index,
+          selectedOptionIndex: question.selectedOptionIndex,
+          isCorrect: question.isCorrect
+        }
+      });
+    }));
+
+    res.status(200).json({ message: 'Questions saved successfully' });
+  } catch (error) {
+    console.error('Error saving questions:', error);
+    res.status(500).json({ message: 'Error saving questions', error: error.message });
+  }
+});
+
+// Endpoint to fetch user-specific questions
+app.get('/user-questions', authenticateToken, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const userQuestions = await prisma.userQuestions.findMany({
+      where: {
+        userId
+      }
+    });
+
+    res.status(200).json(userQuestions);
+  } catch (error) {
+    console.error('Error fetching user questions:', error);
+    res.status(500).json({ message: 'Error fetching user questions' });
+  }
+});
+
 const PORT = 8020;
 
 app.listen(PORT, () => {
