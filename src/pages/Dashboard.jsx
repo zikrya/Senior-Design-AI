f4873ffe3c8ceaff53d834dd6159b4f0d101a7ab
zikrya <77081809+zikrya@users.noreply.github.com>
2023-11-25 15:45:03 -0500

    Minor typo fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -369,7 +369,7 @@ const Dashboard = () => {
            <div className="w-full md:w-1/2 xl:w-1/3 p-6">
                <div className="bg-white border-transparent rounded-lg shadow-xl">
                    <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                       <h2 className="font-bold uppercase text-gray-600">Daily correct and incorrect</h2>
+                       <h2 className="font-bold uppercase text-gray-600">Daily</h2>
                    </div>
                    <div className="p-5">
                        <canvas id="bar-chart" className="chartjs" width="undefined" height="undefined"></canvas>
