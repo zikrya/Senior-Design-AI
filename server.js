90aa64e89ce7a97b6e12a31f6300a77f95487688
zikrya <77081809+zikrya@users.noreply.github.com>
2023-11-25 20:25:56 -0500

    New API Key Again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const app = express();
@@ -27,22 +27,21 @@ app.post("/chat", async (req,res) => {
 
       const openaiPrompt = `Create a JSON array of ${difficulty} level of difficulty 5 multiple-choice questions about '${topic}' suitable for a quiz. Each object in the array should include the question text, an array of options prefixed with 'A', 'B', 'C', and 'D', and the correct option index. Ensure the correct option is at the index specified in the 'correct_option_index' field.`
 
-        // Adjusted the code as per new completion creation method
         const completion = await openai.completions.create({
             model: "text-davinci-003",
-            max_tokens: 512, // Adjust parameter name from max_token to max_tokens
+            max_tokens: 512,
             temperature: 0,
             prompt: openaiPrompt,
         });
-        res.send(completion.choices[0].text); // Adjusted the response accessing method
+        res.send(completion.choices[0].text);
     } catch (error) {
         if (error instanceof OpenAI.APIError) {
-            console.error(error.status);  // Log error status
-            console.error(error.message); // Log error message
-            res.status(error.status).send(error.message); // Send error status and message as response
+            console.error(error.status);
+            console.error(error.message);
+            res.status(error.status).send(error.message);
         } else {
-            console.log(error); // Log other types of errors
-            res.status(500).send('Internal Server Error'); // Send Internal Server Error for other types of errors
+            console.log(error);
+            res.status(500).send('Internal Server Error');
         }
     }
 });
@@ -53,7 +52,6 @@ app.post('/register', async (req, res) => {
   try {
     const { email, firstName, lastName, college, password } = req.body;
 
-    // First, check if the user already exists
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
@@ -62,10 +60,8 @@ app.post('/register', async (req, res) => {
       return res.status(400).json({ message: 'Email already in use' });
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    // Save the new user in the database
     const user = await prisma.user.create({
       data: {
         email,
@@ -87,12 +83,12 @@ app.post('/register', async (req, res) => {
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   console.log("Auth Header:", authHeader);
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.sendStatus(401); // if no token found
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403); // invalid token
+      if (err) return res.sendStatus(403);
       req.user = user;
       next();
   });
@@ -102,7 +98,6 @@ app.post('/login', async (req, res) => {
   try {
       const { email, password } = req.body;
 
-      // Find the user by email
       const user = await prisma.user.findUnique({
           where: { email },
       });
@@ -111,17 +106,14 @@ app.post('/login', async (req, res) => {
           return res.status(404).json({ message: 'User not found' });
       }
 
-      // Compare the password
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
           return res.status(401).json({ message: 'Invalid credentials' });
       }
 
-      // Create a JWT token
       const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
 
-      // Send the token back to the client
       res.json({ message: 'Login successful', token });
 
   } catch (error) {
@@ -130,7 +122,7 @@ app.post('/login', async (req, res) => {
   }
 });
 app.get('/protected-route', authenticateToken, (req, res) => {
-  // Access user information from req.user
+
   res.json({ message: 'Access to protected data!' });
 });
 
@@ -144,7 +136,7 @@ app.post('/logout', (req, res) => {
 
 app.post('/save-questions', authenticateToken, async (req, res) => {
   try {
-    const { questions, topic } = req.body; // Extract topic from the request body
+    const { questions, topic } = req.body;
     const userId = req.user.userId;
 
     await Promise.all(questions.map(async (question) => {
@@ -156,7 +148,7 @@ app.post('/save-questions', authenticateToken, async (req, res) => {
           correctOptionIndex: question.correct_option_index,
           selectedOptionIndex: question.selectedOptionIndex,
           isCorrect: question.isCorrect,
-          topic: topic // Include the topic in each userQuestions record
+          topic: topic
         }
       });
     }));
@@ -167,7 +159,7 @@ app.post('/save-questions', authenticateToken, async (req, res) => {
     res.status(500).json({ message: 'Error saving questions', error: error.message });
   }
 });
-// Endpoint to fetch user-specific questions
+
 app.get('/user-questions', authenticateToken, async (req, res) => {
   try {
     const userId = req.user.userId;
