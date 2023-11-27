03621324c35837446a51d98393e3513cbbea4e8d
zikrya <77081809+zikrya@users.noreply.github.com>
2023-11-26 21:33:52 -0500

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Navbar from './components/navbar'
 import Quiz from './pages/Quiz';
 import React, { useState, useEffect } from 'react';
 import Profile from './pages/Profile';
-import Test from './pages/Test';
+
 
 const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
   return isAuthenticated ? (
